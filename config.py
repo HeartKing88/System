fef5006ef49ded90b84d24cea94352b9955189c7
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-07-24 18:04:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/HeartKing88/Radhey",
+    "https://github.com/HeartKing88/System",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
